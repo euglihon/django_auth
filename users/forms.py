f17b29d907dfe415e720ff43e5939e0e78339c1b
lihon <eug.lihon@hotmail.com>
2020-09-25 13:47:10 +0200

    added email -> CustomUserForm

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,15 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    model = CustomUser
-    fields = UserCreationForm.Meta.fields
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ("username", "email", )
 
 
 class CustomUserChangeForm(UserChangeForm):
-    model = CustomUser
-    fields = UserChangeForm.Meta.fields
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ("username", "email", )
+
